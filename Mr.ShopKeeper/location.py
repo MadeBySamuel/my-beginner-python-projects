0fed5e1cfd08c72d828d1a010c31892e02f01f49
MadeBySamuel <samuel.vojtasak@gmail.com>
2025-08-14 21:13:40 +0200

    add location

---
+++ Mr.ShopKeeper/location.py
@@ -0,0 +1,46 @@
+
+Orava = [{"name_of_district": "Rabčice", "price_of_district": 1000, "level": 1, "status": True },
+        {"name_of_district": "Sihelné", "price_of_district": 1200, "level": 1, "status": True},
+        {"name_of_district": "Rabča", "price_of_district": 2500, "level":2, "status": False},
+        {"name_of_district": "Námestovo", "price_of_district": 5000, "level":3,"status": False},
+        {"name_of_district": "Trstená ", "price_of_district":3500, "level":3,"status": False },
+        {"name_of_district": "Dolný kubín", "price_of_district": 12000, "level":4,"status": False },
+        ]
+
+Zilina = [{"name_of_district": "Vlčince", "price_of_district": 45000, "level": 6, "status": False },]
+
+Bratislava_I = [
+    {"name_of_district": "Staré Mesto", "price_of_district": 350000, "level":20, "status": False },
+]
+
+Bratislava_II = [
+    {"name_of_district":"Ružinov", "price_of_district": 350000, "level":10,"status": False },
+    {"name_of_district":"Vrakuňa", "price_of_district": 350000, "level":12,"status": False },
+    {"name_of_district":"Podunajské Biskupice", "price_of_district": 350000, "level":14, "status": False },
+]
+
+Bratislava_III = [
+    {"name_of_district":"Nové mesto", "price_of_district_of_district": 350000, "level":10,"status": False },
+    {"name_of_district":"Rača", "price_of_district": 350000, "level":13,"status": False },
+    {"name_of_district":"Vajnory", "price_of_district": 350000, "level":15,"status": False },
+]
+Bratislava_IV = [
+    {"name_of_district":"Devínska nová ves", "price_of_district_of_district": 350000, "level":10,"status": False },
+    {"name_of_district":"Zahorská Bystrica", "price_of_district": 350000, "level":12,"status": False },
+    {"name_of_district":"Dúbravka", "price_of_district": 350000, "level":14,"status": False },
+    {"name_of_district": "Lamač", "price_of_district": 350000, "level":15,"status": False },
+    {"name_of_district": "Karlova Ves", "price_of_district": 350000, "level":16,"status": False },
+    {"name_of_district": "Devín", "price_of_district": 350000, "level": 18,"status": False },
+]
+
+Bratislava_V = [
+                {"name_of_district": "Petržalka", "price_of_district": 350000, "level":10,"status": False },
+                {"name_of_district": "Jarovce", "price_of_district": 350000,"letvel":10,"status": False },
+                {"name_of_district":"Rusovce", "price_of_district": 350000,"level":10,"status": False },
+                {"name_of_district":"Čunovo", "price_of_district": 350000, "level":10,"status": False },
+]
+
+
+Bratislava = Bratislava_I + Bratislava_II + Bratislava_III + Bratislava_IV + Bratislava_V
+Slovakia = Bratislava + Orava + Zilina
+
